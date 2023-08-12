8478731918dd4fce46a40c673e87c6f337058551
Arpan Das <arpan.das@klythe.com>
2023-08-12 11:46:27 +0530

    javaScript function edited

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let arr = [
-  { id: 1, name: 'john', age: 18, profession: 'developer' },
-  { id: 2, name: 'jack', age: 20, profession: 'developer' },
-  { id: 3, name: 'karen', age: 19, profession: 'admin' },
+  { id: 1, name: "john", age: 18, profession: "developer" },
+  { id: 2, name: "jack", age: 20, profession: "developer" },
+  { id: 3, name: "karen", age: 19, profession: "admin" },
 ];
 
 // Don't worry about consoling these functions, they are already being called on the button clicks.
@@ -9,18 +9,39 @@ let arr = [
 
 function PrintDeveloper() {
   //Write your code here , just console.log
+  arr.forEach((i) => {
+    if (i.profession === "developer") {
+      console.log(i.name);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newObject = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newObject);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].profession === "admin") {
+      arr.splice(i);
+    }
+  }
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const arr1 = [
+    { id: "5", name: "Arpan", age: 25, profession: "developer" },
+    { id: "6", name: "Krati", age: 22, profession: "Business Admin" },
+    { id: "7", name: "Anjali", age: 24, profession: "developer" },
+  ];
+  const arr2 = arr.concat(arr1);
+  console.log(arr2);
 }
 
 // Here is an example of how functions work,
@@ -28,6 +49,6 @@ function concatenateArray() {
 // If I have a function called consoleArr(), that can directly access arr like this to console it.
 
 function consoleArr() {
-  console.log('Consoling Array Variable', arr);
+  console.log("Consoling Array Variable", arr);
   // Over here I can directly access the variable.
 }
